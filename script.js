2e4b1872174ebd643289e14ee7487413bfcb2099
Diegokraus <diegokraus@gmail.com>
2023-08-04 22:21:05 -0300

    incluindo fetch api

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const button = document.getElementById("convert-button");
 const select = document.getElementById("currency-select");
 
-const dolar = 5.06;
-const euro = 5.56;
-const bitcoin = 141835.09;
 
-const convertValues = () => {
+
+const convertValues = async() => {
   const inputReal = document.getElementById("input-real").value;
   const realValueText = document.getElementById("real-value-text");
   const currencyValueText = document.getElementById("currency-value-text");
 
+  const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+  const dolar = data.USDBRL.high
+  const euro = data.EURBRL.high
+  const bitcoin = data.BTCBRL.high
+ 
   realValueText.innerHTML = currencyValueText.innerHTML = new Intl.NumberFormat(
     "pt-BR",
     {
